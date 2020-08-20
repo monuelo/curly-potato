4533fb8be2787d1497d94ae051e2fe95431b85fd
Héricles <hericles.me@gmail.com>
2020-08-19 23:03:40 -0300

    add linear algebra folder

---
+++ linear-algebra/library/vector.py
@@ -0,0 +1,32 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+    def plus(self, v):
+        new_coords = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coords)
+
+    def minus(self, v):
+        new_coords = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coords)
+
+    def scalar(self, c):
+        new_coords = [x*c for x self.coordinates]
+        return Vector(new_coords)
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
